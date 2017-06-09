456536d0300d79812e2d67b156d6bc6a818b555e
Masao Takaku <tmasao@acm.org>
2017-06-09 16:23:46 +0000

    omit strip for lines.

--- catttl.rb
+++ catttl.rb
@@ -9,7 +9,7 @@ ARGV.each do |file|
   load_turtle(file)
   filename = find_turtle(file)
   File.readlines(filename).each do |line|
-    line = line.chomp.strip
+    line = line.chomp
     next if line.empty?
     if line =~ /^@prefix\s+/
       prefix << line.gsub(/\s+/, " ").strip
