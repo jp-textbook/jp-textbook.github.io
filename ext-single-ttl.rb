dcd19aa6c97573981bf73ef0e1494a7936360c27
Masao Takaku <tmasao@acm.org>
2018-05-18 15:28:19 +0900

    add script for making a single ttl file for each textbook resource. cf.

---
+++ ext-single-ttl.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require "sparql"
+require_relative "util.rb"
+
+include Textbook
+data = load_turtle(find_turtle("textbook.ttl"))
+file = find_turtle("all.ttl")
+STDERR.puts "loading #{file}..."
+g = RDF::Graph.load(file, format:  :turtle)
+
+PREFIX = /\Ahttps:\/\/w3id.org\/jp-textbook\//
+data.each do |key, val|
+  next if not key =~ PREFIX
+  p key
+  file = key.sub(PREFIX, "")
+  file << ".ttl"
+  p file
+  sparql = SPARQL.parse("
+  prefix textbook:  <https://w3id.org/jp-textbook/>
+  DESCRIBE <#{key}>
+  ")
+  g2 = sparql.execute(g)
+  open(file, "w") do |io|
+    io.puts g2.dump(:turtle)
+  end
+end
