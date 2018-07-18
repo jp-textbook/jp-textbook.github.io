bb73d6809ab1e0169f06156a65d3f591a75b7225
Masao Takaku <tmasao@acm.org>
2018-07-18 19:06:45 +0900

    add Catalogue English pages. #218

--- catalogue.rb
+++ catalogue.rb
@@ -7,18 +7,23 @@ require_relative "util.rb"
 
 include Textbook
 data = load_turtle("catalogue.ttl")
+school_data = load_turtle("school.ttl")
 sitemap = Sitemap.new
 template = PageTemplate.new("template/catalogue.html.erb")
+template_en = PageTemplate.new("template/catalogue.html.en.erb")
 
 data.each do |uri, v|
+  school =  v["https://w3id.org/jp-textbook/school"].first
   param = {
     uri: uri,
     style: "../../style.css",
     name: v["http://schema.org/name"][:ja],
+    name_en: v["http://schema.org/name"][:en],
     name_yomi: v["http://schema.org/name"][:"ja-hira"],
     datePublished: v["http://schema.org/datePublished"].first,
     usageYear: v["https://w3id.org/jp-textbook/usageYear"].first,
-    school: v["https://w3id.org/jp-textbook/school"].first,
+    school_name: school_data[school]["http://schema.org/name"][:ja],
+    school_name_en: school_data[school]["http://schema.org/name"][:en],
     url: v["http://schema.org/url"],
     seeAlso: v["http://www.w3.org/2000/01/rdf-schema#seeAlso"],
     callNumber: v["http://dl.nier.go.jp/library/vocab/callNumber"].first,
@@ -31,6 +36,14 @@ data.each do |uri, v|
   FileUtils.mkdir_p(dir) if not File.exist?(dir)
   open(file, "w") do |io|
     io.print template.to_html(param)
+  end
+  sitemap << file
+  file = File.join("en", file)
+  param[:style] = File.join("..", param[:style])
+  dir = File.dirname(file)
+  FileUtils.mkdir_p(dir) if not File.exist?(dir)
+  open(file, "w") do |io|
+    io.print template_en.to_html(param, :en)
   end
   sitemap << file
 end
