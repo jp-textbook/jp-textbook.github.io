1fd61eea49a4896e7a055b72a883463602576b8e
Masao Takaku <tmasao@acm.org>
2018-07-21 06:42:20 +0900

    mkdir_p for non-exist dir. #192

--- ext-single-ttl.rb
+++ ext-single-ttl.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require "fileutils"
 require "sparql"
 require 'ruby-progressbar'
 require_relative "util.rb"
@@ -19,6 +20,8 @@ g.subjects.each.with_progressbar(format: "%a %e %P% Processed: %c from %C") do |
   file << ".ttl"
   sparql = SPARQL.parse("DESCRIBE <#{uri}>")
   g2 = sparql.execute(g)
+  dir = File.dirname(file)
+  FileUtils.mkdir_p(dir) if not File.exist? dir
   open(file, "w") do |io|
     io.puts g2.dump(:turtle).strip
   end
