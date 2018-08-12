d7c6109137850974acb42c4dec1497bdc2817be7
Masao Takaku <tmasao@acm.org>
2018-08-12 10:23:12 +0900

    use RDF::Turtle::Writer.buffer

--- ext-single-ttl.rb
+++ ext-single-ttl.rb
@@ -18,11 +18,23 @@ g.subjects.each.with_progressbar(format: "%a %e %P% Processed: %c from %C") do |
   next if not uri =~ PREFIX
   file = uri.sub(PREFIX, "")
   file << ".ttl"
-  sparql = SPARQL.parse("DESCRIBE <#{uri}>")
-  g2 = sparql.execute(g)
   dir = File.dirname(file)
   FileUtils.mkdir_p(dir) if not File.exist? dir
+  str = RDF::Turtle::Writer.buffer do |writer|
+    g.query([subject, nil, nil]).sort_by do |statement|
+      [ statement.predicate, statement.object ]
+    end.each do |statement|
+      writer << statement
+      if statement.object.node?
+        g.query([statement.object, nil, nil]).sort_by do |s|
+          [ s.predicate, s.object ]
+        end.each do |s|
+          writer << s
+        end
+      end
+    end
+  end
   open(file, "w") do |io|
-    io.puts g2.dump(:turtle).strip
+    io.print str.strip
   end
 end
