40f596dd3c47cb55a7e62c7981a88d0bdb0caecd
Masao Takaku <tmasao@acm.org>
2018-08-12 11:32:22 +0900

    use original formatter. #270

--- ext-single-ttl.rb
+++ ext-single-ttl.rb
@@ -1,17 +1,55 @@
 #!/usr/bin/env ruby
 
 require "fileutils"
-require "sparql"
 require 'ruby-progressbar'
+require "rdf"
+require "rdf/ntriples"
+require "rdf/turtle"
 require_relative "util.rb"
 
 using ProgressBar::Refinements::Enumerator
+
+def format(graph, subject, depth = 1)
+  turtle = RDF::Turtle::Writer.new
+  result = ""
+  if subject.iri?
+    result << "<#{subject}>\n#{"  "*depth}"
+  else
+    result << "[\n#{"  "*depth}"
+  end
+  result << graph.query([subject, nil, nil]).predicates.sort.map do |predicate|
+    str = "<#{predicate}> "
+    str << graph.query([subject, predicate, nil]).objects.sort_by do |object|
+      case object
+      when RDF::Node
+        graph.query([object, nil, nil]).statements.map{|e| [ e.predicate, e.object ] }
+      else
+        object
+      end
+    end.map do |object|
+      if object.resource? and not object.iri? # blank node:
+        format(graph, object, depth + 1)
+      else
+        case object
+        when RDF::URI
+          turtle.format_uri(object)
+        else
+          turtle.format_literal(object)
+        end
+      end
+    end.join(", ")
+    str
+  end.join(";\n#{"  "*depth}")
+  result << " ." if subject.iri?
+  result << "\n"
+  result << "#{"  "*(depth-1)}]" if not subject.iri?
+  result
+end
 
 include Textbook
 file = find_turtle("all.ttl")
 STDERR.puts "loading #{file}..."
 g = RDF::Graph.load(file, format:  :turtle)
-
 PREFIX = /\Ahttps:\/\/w3id.org\/jp-textbook\//
 g.subjects.each.with_progressbar(format: "%a %e %P% Processed: %c from %C") do |subject|
   uri = subject.to_s
@@ -20,21 +58,8 @@ g.subjects.each.with_progressbar(format: "%a %e %P% Processed: %c from %C") do |
   file << ".ttl"
   dir = File.dirname(file)
   FileUtils.mkdir_p(dir) if not File.exist? dir
-  str = RDF::Turtle::Writer.buffer do |writer|
-    g.query([subject, nil, nil]).sort_by do |statement|
-      [ statement.predicate, statement.object ]
-    end.each do |statement|
-      writer << statement
-      if statement.object.node?
-        g.query([statement.object, nil, nil]).sort_by do |s|
-          [ s.predicate, s.object ]
-        end.each do |s|
-          writer << s
-        end
-      end
-    end
-  end
+  str = format(g, subject)
   open(file, "w") do |io|
-    io.print str.strip
+    io.puts str.strip
   end
 end
