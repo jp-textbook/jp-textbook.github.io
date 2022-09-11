e1867ad3d307641ef8d22df1876d55f4d81c9dbc
Masao Takaku <tmasao@acm.org>
2022-09-11 13:37:06 +0900

    optimize check script

--- check.rb
+++ check.rb
@@ -1,25 +1,29 @@
 #!/usr/bin/env ruby
 
+require "ttl2html"
 require_relative "util.rb"
 
 if $0 == __FILE__
   include Textbook
   filename = find_turtle("all.ttl")
+  ttl2html = TTL2HTML::App.new
+  data = ttl2html.load_turtle(filename)
   STDERR.puts "loading #{filename}..."
   klass = File
   klass = Zlib::GzipReader if filename =~ /\.ttl\.gz\z/
-  #g = RDF::Graph.load(filename, format: :turtle)
   klass.open(filename) do |io|
     reader = RDF::Reader.for(:turtle).new(io)
     g = reader.statements
-    missing = ( g.subjects - g.objects - g.predicates ).select{|e|
+    missing = ( g.subjects - g.objects - g.predicates ).select do |e|
       e.is_a?(RDF::URI) and e.to_s.match(BASE_URI)
-    }
+    end
     if not missing.empty?
       puts "Missing usage for subject(s):"
       missing.sort.each do |subject|
-        rdf_type = g.query([subject, RDF::URI("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), nil]).first.object
-        next if rdf_type == RDF::URI("https://w3id.org/jp-textbook/Textbook")
+        rdf_type = data[subject.to_s][RDF.type.to_s]&.first
+        #rdf_type = g.first_object(subject: subject, predicate: RDF.type)
+        next if rdf_type and rdf_type == RDF::URI("https://w3id.org/jp-textbook/Textbook")
+        next if rdf_type and rdf_type == RDF::URI("https://w3id.org/jp-textbook/TeachingUnit")
         p subject
       end
     end
