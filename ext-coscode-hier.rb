7b7052a0660cc85e7c5f7981a1fc1a94f40402c5
Masao Takaku <tmasao@acm.org>
2024-12-31 02:13:08 +0900

    not to use SPARQL... almost done. #591 #620

--- ext-coscode-hier.rb
+++ ext-coscode-hier.rb
@@ -6,6 +6,8 @@ require "json"
 require "roo"
 require "pp"
 
+require "ttl2html"
+
 ENDPOINT = "https://dydra.com/masao/jp-cos/sparql"
 SPARQL = <<EOF
 PREFIX schema: <http://schema.org/>
@@ -24,6 +26,35 @@ class DataCache
     @@cache[key]
   end
 end
+
+class App
+  include TTL2HTML
+  def initialize
+    @ttl2html = TTL2HTML::App.new
+    file = find_turtle("../jp-cos.github.io/all")
+    @data = @ttl2html.load_turtle(file)
+  end
+  def expand_coscode_hierarchy(coscodes)
+    results = []
+    return [] if coscodes.nil? or coscodes.empty?
+    coscodes.strip.split(/,/).each do |coscode|
+      next if coscode.nil? or coscode.empty?
+      results << coscode
+      d = @data["https://w3id.org/jp-cos/#{coscode}"]
+      if d.nil?
+        STDERR.puts "WARN: not found: #{coscode}"
+        next
+      end
+      if d and d["http://schema.org/hasPart"]
+        d["http://schema.org/hasPart"].each do |child|
+          results << expand_coscode_hierarchy(child.to_s.split(/\//).last)
+        end
+      end
+    end
+    results.flatten.sort.uniq
+  end
+end
+
 def expand_coscode_hier(coscodes)
   results = []
   return [] if coscodes.nil? or coscodes.empty?
@@ -48,17 +79,22 @@ def expand_coscode_hier(coscodes)
   results.flatten.sort.uniq
 end
 
+$0 = File.expand_path($0, __dir__)
 if $0 == __FILE__
   if ARGV.size < 1
     puts "USAGE #$0 filename [sheetname]"
     exit
   end
   xlsx = Roo::Spreadsheet.open(ARGV[0])
+  if ARGV[1]
+    xlsx.default_sheet = ARGV[1]
+  end
   skip = true
   cos_idx = nil
+  app = App.new
   xlsx.each_row_streaming(pad_cels: true) do |row|
     if skip
-      if row[0].to_s == "No"
+      if row[0].to_s == "No" or row[0].to_s =~ /ID$/
         skip = false
         cos_idx = row.index{|e| e.to_s == "cosコード" }
       end
@@ -67,7 +103,7 @@ if $0 == __FILE__
     next if row.empty? or row[0].empty?
     coscode = row.find(""){|e| e.coordinate[1] == cos_idx + 1 }.to_s
     if coscode and not coscode.empty?
-      expanded = expand_coscode_hier(coscode).join(",")
+      expanded = app.expand_coscode_hierarchy(coscode).join(",")
       puts [ row[0], coscode, expanded ].join("\t")
     else
       puts row[0]
