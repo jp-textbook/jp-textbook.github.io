1eb236b456b67bac525cda62bc2f1420d4204c7a
Masao Takaku <tmasao@acm.org>
2025-02-03 18:51:38 +0900

    update expand script. #635

--- ext-coscode-hier.rb
+++ ext-coscode-hier.rb
@@ -96,7 +96,8 @@ if $0 == __FILE__
     if skip
       if row[0].to_s == "No" or row[0].to_s =~ /ID$/
         skip = false
-        cos_idx = row.index{|e| e.to_s == "cosコード" }
+        cos_idx = row.index{|e| e.to_s == "cosコード" or e.to_s == "オリジナルcos" }
+        raise "cos_idx not found: one of the header must have either the values of \"cosコード\" or \"オリジナルcos\"" if cos_idx.nil?
       end
       next
     end
