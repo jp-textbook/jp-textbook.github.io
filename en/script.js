dcb4a5c86aac6d500548617c30804beb654548db
Masao Takaku <tmasao@acm.org>
2025-02-05 00:06:47 +0900

    add javascript for English site. #632

---
+++ en/script.js
@@ -0,0 +1,127 @@
+function get_nhk4school(cscode, page = 1) {
+  /// NHK for School APIにアクセス
+  const apiKey = 'Ul2TEbnk3A9AeaTxXHkHjXoR3kdTGmAj';
+  const api = `https://api.nhk.or.jp/school/v1/nfsvideos/cscode/${cscode}?apikey=${apiKey}&page=${page}`;
+  fetch(api)
+  .then(response => {
+      return response.json();
+  })
+  .then(data => {
+      render_nhk4school(data, cscode);
+  })
+  .catch(function (error) {
+      console.log(`Failed: ${error}`);
+      render_nhk4school_results("<p>Load failed.</p>", cscode, page, false);
+  });
+
+  function render_nhk4school_results(html, cscode, page, success = true) {
+      //結果を表示
+      let elmResult = document.getElementById(`nhk4school-list-${cscode}-${page}`);
+      elmResult.innerHTML = html;
+      if (success) {
+          let elmButton = document.getElementById(`nhk4school-button-${cscode}-${page}`);
+          elmButton.disabled = true;
+          if (page == 1) {
+            let buttonText = `
+            <button class="btn btn-outline-info btn-sm nhk4chool-toggle" type="button" data-toggle="collapse" data-target="#nhk4school-list-${cscode}-1" aria-controls="#nhk4school-list-${cscode}-1">
+              Expand/Collapse List
+            </button>
+            `;
+            elmButton.insertAdjacentHTML("afterend", buttonText);
+            console.log(document.getElementById(`nhk4school-list-${cscode}-1`));
+          }
+      }
+  }
+
+  function render_nhk4school(data, cscode) {
+      console.log(data);
+      console.log(data['counts']);
+      let html = ''; //出力するHTMLテキストを入れる変数
+      if (data['error'] || !data['counts']) {
+          html += "No results found.";
+      } else {
+          //取得結果の概要を表示
+          const pageStart = data['perPage'] * (data['page']-1) + 1;
+          const pageEnd   = pageStart + data['result'].length - 1;
+          html += `<p>Videos by NHK for School: Showing ${pageStart} - ${pageEnd} of ${data['counts']} results:</p>`;
+          //コンテンツ一覧を表示
+          html += `<div class="row">`;
+          for (let i = 0; i < data['result'].length; i++){
+              let seriesTitle = "";
+              if (data['result'][i]['url'] && data['result'][i]['about'] && data['result'][i]['about']['nfsSeriesName']) {
+                  seriesTitle = ` (${data['result'][i]['about']['nfsSeriesName']})`;
+              }
+              const result = `
+                  <div class="col-md-4">
+                    <div class="card shadow-sm">
+                      <a href="${data['result'][i]['url']}"><img class="bd-placeholder-img card-img-top" width="100%" src="${data['result'][i]['thumbnailUrl']}"></img></a>
+                      <div class="card-body small">
+                        <p class="card-text">${data['result'][i]['name']}${seriesTitle}</p>
+                        <p class="card-text">${data['result'][i]['description']}</p>
+                        <button type="button" class="btn btn-outline-light"><a href="${data['result'][i]['url']}">View</a></button>
+                      </div>
+                    </div>
+                  </div>
+              `;
+              //console.log(result);
+              html += result;
+          }
+          html += '</div>';
+      }
+      if (data['page'] < data['totalPages']) {
+          const nextPage = data['page'] + 1;
+          html += `
+              <button id="nhk4school-button-${cscode}-${nextPage}" type="button" class="btn btn-outline-info btn-sm btn-nhk4school p-1" onclick="get_nhk4school('${data['queryData']['curriculumStandardCode']}', ${nextPage})">
+              Load next ${data['perPage']} results <i class="bi bi-search"></i>
+              </button>
+              <div id="nhk4school-list-${cscode}-${nextPage}" />
+          `;
+      }
+      render_nhk4school_results(html, cscode, data['page']);
+  };
+};
+
+let fetcher = new window.ldfetch();
+function fetch_jp_cos(url, elem) {
+  let main = async function () {
+    let objects = await fetcher.get(url).then(response => {
+      return fetcher.frame(response.triples, {'@graph':{}});
+    });
+    console.log(objects);
+    objects["@graph"].forEach(e => {
+      if (e["@id"] == url) {
+        let parent = elem.parentNode;
+        let cscode = e["http://purl.org/dc/terms/identifier"]["@value"];
+        let cos = e["https://w3id.org/jp-cos/courseOfStudy"]["@id"];
+        parent.innerHTML += ` <span class="sectionNumberHierarchy">${e["https://w3id.org/jp-cos/sectionNumberHierarchy"]["@value"]}</span>`;
+        parent.innerHTML += `<br><span class="sectionText">${e["https://w3id.org/jp-cos/sectionText"]["@value"]}</span>`;
+        //console.log(parent);
+        switch (cos) {
+          case "https://w3id.org/jp-cos/Elementary/2017":
+          case "https://w3id.org/jp-cos/LowerSecondary/2017":
+          case "https://w3id.org/jp-cos/UpperSecondary/2018":
+            parent.innerHTML += `
+            <button id="nhk4school-button-${cscode}-1" class="btn btn-outline-info btn-sm btn-nhk4school p-1"
+              onclick="get_nhk4school('${cscode}')">
+              Search videos from "NHK for School" <i class="bi bi-search"></i>
+            </button>
+            <div id="nhk4school-list-${cscode}-1" class="collapse show"></div>
+            `;
+            break;
+        }
+    }
+    });
+  }
+  try {
+    main();
+  } catch (e) {
+    console.error(e);
+  }
+}
+$("dl.row dd a").each(function(link){
+  //let url = "https://w3id.org/jp-cos/8220233111000000";
+  let url = this.href.toString();
+  if (url.startsWith("https://w3id.org/jp-cos/")) {
+    fetch_jp_cos(url, this);
+  }
+});
