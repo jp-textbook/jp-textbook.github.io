d12fc68771833b3eaf444722428daf3c821067e1
Masao Takaku <tmasao@acm.org>
2025-03-01 12:26:35 +0900

    updated

--- ext-coscode-hier.rb
+++ ext-coscode-hier.rb
@@ -9,25 +9,6 @@ require "pp"
 require "ttl2html"
 require_relative "util.rb"
 
-ENDPOINT = "https://dydra.com/masao/jp-cos/sparql"
-SPARQL = <<EOF
-PREFIX schema: <http://schema.org/>
-PREFIX cs: <https://w3id.org/jp-cos/>
-select * where { 
-  cs:%s schema:hasPart+ ?child.
-} ORDER BY ASC(?child)
-EOF
-
-class DataCache
-  @@cache = {}
-  def self.[]=(key, value)
-    @@cache[key] = value
-  end
-  def self.[](key)
-    @@cache[key]
-  end
-end
-
 class App < TTL2HTML::App
   include TTL2HTML
   def initialize
@@ -60,7 +41,7 @@ class App < TTL2HTML::App
     while not coscodes.empty?
       coscode = coscodes.pop
       parent = @data_inverse["https://w3id.org/jp-cos/#{coscode}"]
-      if parent["http://schema.org/hasPart"]
+      if parent and parent["http://schema.org/hasPart"]
         parent["http://schema.org/hasPart"].each do |e|
           children = @data[e]["http://schema.org/hasPart"].map{|c| c.last_part }
           #p [:children, children]
@@ -77,33 +58,8 @@ class App < TTL2HTML::App
   end
 end
 
-def expand_coscode_hier(coscodes)
-  results = []
-  return [] if coscodes.nil? or coscodes.empty?
-  coscodes.strip.split(/,/).each do |coscode|
-    results << "https://w3id.org/jp-cos/#{coscode}"
-    if not DataCache[coscode]
-      sparql = SPARQL % coscode
-      uri = ENDPOINT + "?" + URI.encode_www_form(query: sparql)
-      STDERR.puts uri
-      body = Net::HTTP.get(URI.parse(uri))
-      data = JSON.load(body)
-      data["results"]["bindings"].each do |bindings|
-        results << bindings["child"]["value"]
-      end
-      DataCache[coscode] = results
-      sleep(0.3)
-    else
-      STDERR.puts "cache hit!"
-      results << DataCache[coscode]
-    end
-  end
-  results.flatten.sort.uniq
-end
-
 $0 = File.expand_path($0, __dir__)
 if $0 == __FILE__
-  app = App.new
   if ARGV.size < 1
     puts "USAGE #$0 filename [sheetname]"
     exit
@@ -125,7 +81,7 @@ if $0 == __FILE__
       next
     end
     next if row.empty? or row[0].empty?
-    coscode = row.find(""){|e| e.coordinate[1] == cos_idx + 1 }.to_s
+    coscode = row.find{|e| e.coordinate[1] == cos_idx + 1 }.to_s
     if coscode and not coscode.empty?
       expanded = app.expand_coscode_hierarchy(coscode)
       expanded = app.expand_coscode_hierarchy_upward(expanded)
